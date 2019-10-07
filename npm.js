07bb53537fe078bc0620e1f423765d3e8cf44def
AlisonYoon <politescarlett1@gmail.com>
2019-10-07 14:10:18 +0100

    what is npm

--- npm.js
+++ npm.js
@@ -1,1 +1,4 @@
 // NPM : node package manager
+// publicly available packages
+// Add node module in you .gitignore
+// in package.json file, it has all the dependencies a project needs. So when do $npm install, this will get all the npm packages it needs
