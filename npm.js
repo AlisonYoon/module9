5cfced0f7bea1a9060d49b62bd6dc37bec396054
AlisonYoon <politescarlett1@gmail.com>
2019-10-07 14:11:25 +0100

    how to create NPM package

--- npm.js
+++ npm.js
@@ -2,3 +2,9 @@
 // publicly available packages
 // Add node module in you .gitignore
 // in package.json file, it has all the dependencies a project needs. So when do $npm install, this will get all the npm packages it needs
+
+//To create a new NPM package:
+//npm init
+//add index.js + code
+//add readme.md
+//npm publish
