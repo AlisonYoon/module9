72ed0c968fe1432aa2170acb7af25bc4fcced06d
AlisonYoon <politescarlett1@gmail.com>
2019-10-08 15:37:13 +0100

    how to update MontoDB

---
+++ MongoDB/update.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+const jsonParser = bodyParser.json()
+
+const MongoClient = require('mongodb').MongoClient
+
+const url = 'mongodb://localhost:27017'
+
+let updateDataInDb = (db, newData) => {
+    let collection = db. collection('people')
+    collection.updateOne({"name" : "Bob"}, { $set: newData }, (err, result) => {
+        console.log("Updated the document")
+    })
+}
+
+app.put('/people', jsonParser, (req, res) => {
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+        console.log('Connected correctly to MongoDb')
+
+        let db = client.db('axolotls')
+        let newData = req.body // write something in the Postman to update with data
+        updateDataInDb(db, newData)
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+})
