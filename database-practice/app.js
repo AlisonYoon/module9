404f0aa444addf6cd5398817bf3a28ce4c3bb9d4
AlisonYoon <politescarlett1@gmail.com>
2019-10-08 13:12:23 +0100

    select more columns from the mysql db

--- database-practice/app.js
+++ database-practice/app.js
@@ -7,15 +7,15 @@ var port = 3000
 var connection = mysql.createConnection({
     host: '192.168.20.20',
     user: 'root',
-    database: 'cats'
+    database: 'academyPortal'
 })
 
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
 
-app.get('/breed', function(req, res) {
+app.get('/applicants', function(req, res) {
     connection.connect()
-    connection.query('SELECT `breed` FROM `breed`;', function(error, results, fields) {
+    connection.query('SELECT `name`, `email` FROM `applicants`;', function(error, results, fields) {
         //var breedList = res.json({data: results})
         res.render('home', {data: results})
     })
